a953d2b2d4a743cf7b0f9fca019f111b1020f999
naonao-aaa <naonao1814naonao@gmail.com>
2024-04-07 18:17:42 +0900

    [update]ログイン画面

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,18 +1,15 @@
 import axios from "axios";
-import { ChangeEvent, useState } from "react";
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const changeEmail = (e) => {
-    setEmail(e.target.value);
-  };
-  const changePassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleClick = () => {
+  const submitHandler = async (e) => {
+    e.preventDefault();
     const loginParams = { email, password };
     axios
       // CSRF保護の初期化。（ログイン処理を行う前に行うもの！）
@@ -28,35 +25,50 @@ export default function Login() {
           })
           .then((response) => {
             console.log(response.data);
+          })
+          .catch((error) => {
+            console.log(error.response.data.message);
           });
-      });
-  };
-
-  // SPA認証済みではないとアクセスできないAPI
-  const handleUserClick = () => {
-    axios
-      .get("http://localhost:8080/api/user", { withCredentials: true })
-      .then((response) => {
-        console.log(response.data);
       });
   };
 
   return (
     <>
-      <div>
-        メールアドレス
-        <input onChange={changeEmail} />
-      </div>
-      <div>
-        パスワード
-        <input onChange={changePassword} />
-      </div>
-      <div>
-        <button onClick={handleClick}>ログイン</button>
-      </div>
-      <div>
-        <button onClick={handleUserClick}>ユーザー情報を取得</button>
-      </div>
+      <FormContainer>
+        <h1>ログイン</h1>
+
+        <Form onSubmit={submitHandler}>
+          <Form.Group className="my-2" controlId="email">
+            <Form.Label>メールアドレス</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group className="my-2" controlId="password">
+            <Form.Label>パスワード</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Button type="submit" variant="primary">
+            ログイン
+          </Button>
+        </Form>
+
+        <Row className="py-3">
+          <Col>
+            <Link to="/register">アカウントをお持ちではない方はこちら</Link>
+          </Col>
+        </Row>
+      </FormContainer>
     </>
   );
 }
