1bc458c64db65c60d07be716390f5fffe8ffd32d
naonao-aaa <naonao1814naonao@gmail.com>
2024-04-09 08:26:17 +0900

    [add]カート追加機能（「addToCart」という「Reducer」と「Action Creator」を作成）

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -7,7 +7,42 @@ const initialState = localStorage.getItem("cart")
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      // カートに追加したアイテム。
+      const item = action.payload;
+
+      // カート内に、同じidを持つアイテムが既に存在するかどうかを確認する。
+      const existItem = state.cartItems.find((x) => x.id === item.id);
+
+      if (existItem) {
+        // カート内に同じアイテムが既に存在する場合、そのアイテムの数量を更新する。
+        state.cartItems = state.cartItems.map((x) =>
+          x.id === existItem.id ? item : x
+        );
+      } else {
+        // カート内に同じアイテムが存在しない場合は、新しいアイテムとしてカートに追加する。
+        state.cartItems = [...state.cartItems, item];
+      }
+
+      // カート内の全てのアイテムの価格を合計して、state.itemsPriceに代入する。
+      state.itemsPrice = state.cartItems.reduce(
+        (acc, item) => acc + item.price * item.qty,
+        0
+      );
+
+      // 税金の計算(消費税10％)。小数は切り捨てる。
+      state.taxPrice = Math.floor(0.1 * state.itemsPrice);
+
+      // 合計価格の計算。（商品価格 + 税金）
+      state.totalPrice = Number(state.itemsPrice) + Number(state.taxPrice);
+
+      // ローカルストレージへの保存。
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+  },
 });
+
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
