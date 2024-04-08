725d68e744fa4f0ef4ffe3cc70a7acd51c3448a7
naonao-aaa <naonao1814naonao@gmail.com>
2024-04-09 08:37:04 +0900

    [refactor]処理をutilsファイルに切り出す。

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
@@ -12,33 +13,7 @@ const cartSlice = createSlice({
       // カートに追加したアイテム。
       const item = action.payload;
 
-      // カート内に、同じidを持つアイテムが既に存在するかどうかを確認する。
-      const existItem = state.cartItems.find((x) => x.id === item.id);
-
-      if (existItem) {
-        // カート内に同じアイテムが既に存在する場合、そのアイテムの数量を更新する。
-        state.cartItems = state.cartItems.map((x) =>
-          x.id === existItem.id ? item : x
-        );
-      } else {
-        // カート内に同じアイテムが存在しない場合は、新しいアイテムとしてカートに追加する。
-        state.cartItems = [...state.cartItems, item];
-      }
-
-      // カート内の全てのアイテムの価格を合計して、state.itemsPriceに代入する。
-      state.itemsPrice = state.cartItems.reduce(
-        (acc, item) => acc + item.price * item.qty,
-        0
-      );
-
-      // 税金の計算(消費税10％)。小数は切り捨てる。
-      state.taxPrice = Math.floor(0.1 * state.itemsPrice);
-
-      // 合計価格の計算。（商品価格 + 税金）
-      state.totalPrice = Number(state.itemsPrice) + Number(state.taxPrice);
-
-      // ローカルストレージへの保存。
-      localStorage.setItem("cart", JSON.stringify(state));
+      return updateCart(state, item);
     },
   },
 });
