4f0e65aa1dab5246b9c0c13fb0a39cd62db7f8aa
naonao-aaa <naonao1814naonao@gmail.com>
2024-04-08 12:44:49 +0900

    [refactor]productsテーブルとreviewsテーブルのカラムを修正

--- backend/database/migrations/2024_04_07_111719_create_reviews_table.php
+++ backend/database/migrations/2024_04_07_111719_create_reviews_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
             $table->foreignId('product_id')->constrained()->onDelete('cascade');
-            $table->string('name');
+            // $table->string('name');
             $table->unsignedInteger('rating');
             $table->text('comment');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
