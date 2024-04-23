a661b04d6f05ce330b69e2c40434628c097dc731
naonao-aaa <naonao1814naonao@gmail.com>
2024-04-23 12:19:52 +0900

    [refactor]デプロイ後のことを考慮し、.envファイルにLaravel側のサーバーURLを用意

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,3 @@
-export const BASE_URL =
-  process.env.REACT_APP_ENVIRONMENT === "development"
-    ? "http://localhost:8080"
-    : "/";
+export const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const PRODUCTS_URL = "/api/products";
